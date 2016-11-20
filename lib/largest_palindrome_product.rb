ae6cabeb9e11ea3333c1312d2e956aba33efcf00
gregmarquet <marquet.greg@gmail.com>
2016-11-19 22:36:59 -0500

    Done

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,10 @@
-# Implement your procedural solution here!
+
+def largest_palindrome_product
+  result = 0
+  (100...1000).each do |n|
+    (100...1000).each do |n1| 
+      result = (n * n1) if ((n * n1).to_s == (n * n1).to_s.reverse) && n * n1 > result
+    end
+  end
+  result
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,16 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def initialize
+    @range = (100...1000)
+    @result = 0
+  end
+
+  def answer
+    @range.each do |n|
+      @range.each do |n1| 
+        @result = (n * n1) if ((n * n1).to_s == (n * n1).to_s.reverse) && n * n1 > @result
+      end
+    end
+    @result
+  end
+
+end
